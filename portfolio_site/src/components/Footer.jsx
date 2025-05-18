24f7e4b392414c404700df5e5385c153ff78b712
ianschwarzkopf <131293503+ianschwarzkopf@users.noreply.github.com>
2025-05-18 15:12:46 -0400

    footer changes

--- portfolio_site/src/components/Footer.jsx
+++ portfolio_site/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css'
 
 function Footer() {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <p>© 2025 Ian Schwarzkopf</p>
     </footer>
   )
